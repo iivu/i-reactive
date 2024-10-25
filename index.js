243d4d7adb01896472443a2956041c1ffd10844d
Leo Cheung <leozhang621@gmail.com>
2024-10-25 11:05:54 +0800

    refactor：重新设计数据结构，达成target->key-effectFns的映射，准确收集依赖 refactor：重构track/trigger函数

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // 副作用函数桶，保存所有副作用函数
-const bucket = new Set()
+// target -> Map<key, Set<fn>>
+const bucket = new WeakMap()
 // 全局变量，用来保存当前正在执行的副作用函数
 let activeEffect = null
 
@@ -9,19 +10,39 @@ export function effect(fn) {
   fn()
 }
 
+// 跟踪变化
+export function track(target, key) {
+  if (!activeEffect) return
+  // 取得当前响应式数据所有副作用函数的map: target -> Map<key, Set<fn>>
+  let depsMap = bucket.get(target)
+  if (!depsMap) {
+    bucket.put(target, (depsMap = new Map()))
+  }
+  // 取得当前key的副作用函数集合: key -> Set<fn>
+  let deps = depsMap.get(key)
+  if (!deps) {
+    depsMap.put(key, (deps = new Set()))
+  }
+  // 建立联系: target -> key -> effectFn
+  deps.add(activeEffect)
+}
+
+// 变化后触发副作用
+export function trigger(target, key) {
+  bucket.get(target)?.get(key)?.forEach(fn => fn())
+}
+
 const data = { text: 'hello world!' }
 const proxy = new Proxy(data, {
-  get(target, p, receiver) {
+  get(target, key, receiver) {
     // 每当发生读操作，都把当前的副作用函数放到bucket
-    if (activeEffect) {
-      bucket.add((activeEffect))
-    }
-    return target[p]
+    track(target, key)
+    return target[key]
   },
-  set(target, p, newValue, receiver) {
+  set(target, key, newValue, receiver) {
+    target[key] = newValue
     // 每当发生写操作，都把副作用函数执行一遍
-    target[p] = newValue
-    bucket.forEach(fn => fn())
+    trigger(target, key)
     return true
   }
 })
